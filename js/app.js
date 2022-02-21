3c875a554e2afaa6e713bbb661ba4d4caa086a27
Ítalo Araújo <98362702+intalo@users.noreply.github.com>
2022-02-21 20:26:14 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-let navbar_container = document.querySelector('ul#navbar_container');
-let menu_status = 'h'
+let navbar_container = document.querySelector("ul#navbar_container");
+let menu_status = "h";
 
 function showMenu() {
-    if (menu_status == 'v') {
-        navbar_container.style = 'display: none;'
-        menu_status = 'h'
-    } else if (menu_status == 'h') {
-        navbar_container.style = 'display: block;'
-        menu_status = 'v'
+    if (menu_status == "v") {
+        navbar_container.style =
+            "opacity: 0; transition: 0.3s; top: -25px; width: 25vw;";
+        menu_status = "h";
+    } else if (menu_status == "h") {
+        navbar_container.style =
+            "opacity: 1; transition: 0.3s; top: 75px; width: 75vw;";
+        menu_status = "v";
     }
 }
